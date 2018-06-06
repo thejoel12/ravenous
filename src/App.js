674b8c44f4f5611accb7aa2993b06d907ad7edfb
Joel Borchardt <joelborchardt@Joels-MacBook-Pro.local>
2018-06-06 11:11:16 -0400

    having issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList.js';
@@ -7,18 +7,44 @@ import SearchBar from './components/SearchBar/SearchBar.js';
 
 
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
 
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
 
-class App extends Component {
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching yelp with ${term} in ${location}, sorted by ${sortBy}`);
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList />
+        <SearchBar SearchBar={this.searchYelp}/>
+        <BusinessList businesses={businesses}/>
       </div>
     );
   }
+
+
+
 }
 
 export default App;
